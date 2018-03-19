b029c41c1c83a6041e342421aa93d1a1d97979f9
Jonathan Pearman <jpearman1019@gmail.com>
2018-03-19 12:59:27 -0500

    finished modifying css for smaller resolutions

--- request-gif.js
+++ request-gif.js
@@ -1,5 +1,4 @@
 
-
 $(document).ready(function() {
     // register our function as the "callback" to be triggered by the form's submission event
     $("#form-gif-request").submit(fetchAndDisplayGif); // in other words, when the form is submitted, fetchAndDisplayGif() will be executed
@@ -13,30 +12,62 @@ $(document).ready(function() {
  * upon receiving a response from Giphy, updates the DOM to display the new GIF
  */
 function fetchAndDisplayGif(event) {
-    
     // This prevents the form submission from doing what it normally does: send a request (which would cause our page to refresh).
     // Because we will be making our own AJAX request, we dont need to send a normal request and we definitely don't want the page to refresh.
     event.preventDefault();
-    
     // get the user's input text from the DOM
     var searchQuery = ""; // TODO should be e.g. "dance"
+    var riddle = "";
+    $("#riddle-error").text("");
+    $(".border").css("border-color", "black");
+    //$("#gif").attr("hidden", true);
+    //console.log(document.getElementById("gif").hidden)
+    
+    
+    
+    searchQuery = document.getElementById("form-gif-request").elements[0].value;
+    riddle = document.getElementById("form-gif-request").elements[1].value;
+    //console.log(searchQuery)
+    //console.log(riddle)
+
+    if(riddle!=5){
+        $(".border").css("border-color", "red");
+        
+        console.log("working")
+        $("#riddle-error").text("No gifs for you.");
+        return;
+        
+        
+
+    }
 
     // configure a few parameters to attach to our request
     var params = { 
         api_key: "dc6zaTOxFJmzC", 
-        tag : "" // TODO should be e.g. "jackson 5 dance"
+        tag : "jackson 5 " + searchQuery // TODO should be e.g. "jackson 5 dance"
     };
+    console.log(params)
     
     // make an ajax request for a random GIF
     $.ajax({
-        url: "", // TODO where should this request be sent?
+        url: "https://api.giphy.com/v1/gifs/random", // TODO where should this request be sent?
         data: params, // attach those extra parameters onto the request
         success: function(response) {
             // if the response comes back successfully, the code in here will execute.
             
             // jQuery passes us the `response` variable, a regular javascript object created from the JSON the server gave us
-            console.log("we received a response!");
-            console.log(response);
+            
+            //console.log(response.data.bitly_url);
+            var jaxResponse = response.data.image_url
+            var gifUrl = jaxResponse
+            //console.log(gifUrl)
+            document.getElementById("gif").src = gifUrl
+            //console.log(document.getElementById("gif").src)
+            //document.getElementById("gif").hidden = false
+
+            setGifLoadedStatus(true);
+            
+
             
             // TODO
             // 1. set the source attribute of our image to the image_url of the GIF
@@ -53,7 +84,8 @@ function fetchAndDisplayGif(event) {
     
     // TODO
     // give the user a "Loading..." message while they wait
-    
+    $("#feedback").text("Loading");
+    setGifLoadedStatus(false)
 }
 
 
